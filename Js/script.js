af2d410859b8c68646711f24cdd87c8e56e1bb4a
Andres Castiello <andres.g.castiello@gmail.com>
2021-10-09 13:30:45 -0300

    merge con javascript

---
+++ Js/script.js
@@ -0,0 +1,21 @@
+function main(){
+
+    let header={
+
+        expandir(){
+
+           document.querySelector(".header>nav").style.display="flex";
+           document.getElementsByClassName("header__menu")[0].style.display="none";
+        },
+        contraer(){
+
+            document.querySelector(".header>nav").style.display="";
+            document.getElementsByClassName("header__menu")[0].style.display="";
+        }
+    }
+    document.getElementsByClassName("header")[0].addEventListener("click",header.expandir);
+    document.getElementsByClassName("header")[0].addEventListener("mouseleave",header.contraer);
+}
+
+
+window.onload= main;
